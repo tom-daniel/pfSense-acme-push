55ce932df35735b1e4e8be40d2361188f1487e44
Tom Daniel <tom@the-daniels.co.uk>
2020-02-12 17:01:55 +0000

    Re wrote to reflect  correct path of certs within pfsense

--- pushcerts-cer-key.sh
+++ pushcerts-cer-key.sh
@@ -8,33 +8,25 @@
 #Set the variables
 
 #CERT_NAME is the the name of the certificate as set in pfSense
-CERT_NAME=server.domain-lets-encrypt-cert
+CERT_NAME=host.domain.co.uk-lets-encrypt-cert
+
+#DOMAIN_NAME is the the domain name of the certificate 
+DOMAIN_NAME=host.domain.co.uk
 
 #DESTINATION_SERVER is the FQDN or ipaddress of the server the certificates should be pushed to
-DESTINATION_SERVER=server.domain.co.uk
+DESTINATION_SERVER=$DOMAIN_NAME
 
 #USER is the username of a user with access on the remote server (you may wish to set up a pfsenseacme user on both pfsense and the destination server specificaly for this task)
-USER=pfsenseacme
+USER=root
 
 #DESTINATION_PATH is the directory in which you wish the final certificate to be placed (the example below if for Cockpit on Ubuntu)
 DESTINATION_PATH=/etc/cockpit/ws-certs.d/
 
-#DESTINATION_CER_NAME and DESTINATION_KEY_NAME are what you wish for the certificate to be named, you can use the $CERT_NAME variable here to name it the same  
-DESTINATION_CER_NAME=$CERT_NAME.cer
-DESTINATION_KEY_NAME=$CERT_NAME.key
+#DESTINATION_CER_NAME is what you wish for the certificate to be named, you can use the $CERT_NAME variable here to name it the same   
+DESTINATION_CER_NAME=$DOMAIN_NAME.cer
+DESTINATION_CER_NAME=$DOMAIN_NAME.key
 
-#Change ownership of the files to ensure they able to be updated on the next renewal
-chown $USER /cf/conf/acme/$CERT_NAME.crt
-chmod 660 /cf/conf/acme/$CERT_NAME.crt
-chown $USER /cf/conf/acme/$CERT_NAME.key
-chmod 660 /cf/conf/acme/$CERT_NAME.key
 
-#Transfer the files
-scp /cf/conf/acme/$CERT_NAME.crt $USER@$DESTINATION_SERVER:$DESTINATION_PATH$DESTINATION_CER_NAME
-scp /cf/conf/acme/$CERT_NAME.key $USER@$DESTINATION_SERVER:$DESTINATION_PATH$DESTINATION_KEY_NAME
-
-#Revert ownership and permissions of the files back to root
-chown root /cf/conf/acme/$CERT_NAME.crt
-chmod 640 /cf/conf/acme/$CERT_NAME.crt
-chown root /cf/conf/acme/$CERT_NAME.key
-chmod 640 /cf/conf/acme/$CERT_NAME.key
+#transfer the file
+scp /tmp/acme/$CERT_NAME/$DOMAIN_NAME/$DOMAIN_NAME.cer $USER@$DESTINATION_SERVER:$DESTINATION_PATH$DESTINATION_CER_NAME
+scp /tmp/acme/$CERT_NAME/$DOMAIN_NAME/$DOMAIN_NAME.key $USER@$DESTINATION_SERVER:$DESTINATION_PATH$DESTINATION_KEY_NAME
--- pushcerts-cert.sh
+++ pushcerts-cert.sh
@@ -8,30 +8,29 @@
 #Set the variables
 
 #CERT_NAME is the the name of the certificate as set in pfSense
-CERT_NAME=server.domain-lets-encrypt-cert
+CERT_NAME=host.domain.co.uk-lets-encrypt-cert
+
+#DOMAIN_NAME is the the domain name of the certificate 
+DOMAIN_NAME=host.domain.co.uk
 
 #DESTINATION_SERVER is the FQDN or ipaddress of the server the certificates should be pushed to
-DESTINATION_SERVER=server.domain.co.uk
+DESTINATION_SERVER=$DOMAIN_NAME
 
 #USER is the username of a user with access on the remote server (you may wish to set up a pfsenseacme user on both pfsense and the destination server specificaly for this task)
-USER=pfsenseacme
+USER=root
 
 #DESTINATION_PATH is the directory in which you wish the final certificate to be placed (the example below if for Cockpit on Ubuntu)
 DESTINATION_PATH=/etc/cockpit/ws-certs.d/
 
 #DESTINATION_CER_NAME is what you wish for the certificate to be named, you can use the $CERT_NAME variable here to name it the same   
-DESTINATION_CER_NAME=$CERT_NAME.cert
+DESTINATION_CER_NAME=$DOMAIN_NAME.cert
 
 #merge the files
-cat /cf/conf/acme/$CERT_NAME.crt >> /cf/conf/acme/$CERT_NAME.cert
-cat /cf/conf/acme/$CERT_NAME.key >> /cf/conf/acme/$CERT_NAME.cert
-
-#Change ownership of the file to ensure it is able to be updated on the next renewal
-chown $USER /cf/conf/acme/$CERT_NAME.cert
-chmod 660 /cf/conf/acme/$CERT_NAME.cert
+cat /tmp/acme/$CERT_NAME/$DOMAIN_NAME/$DOMAIN_NAME.cer >> /tmp/acme/$CERT_NAME/$DOMAIN_NAME/$DOMAIN_NAME.cert
+cat /tmp/acme/$CERT_NAME/$DOMAIN_NAME/$DOMAIN_NAME.key >> /tmp/acme/$CERT_NAME/$DOMAIN_NAME/$DOMAIN_NAME.cert
 
 #transfer the file
-scp /cf/conf/acme/$CERT_NAME.cert $USER@$DESTINATION_SERVER:$DESTINATION_PATH$DESTINATION_CER_NAME
+scp /tmp/acme/$CERT_NAME/$DOMAIN_NAME/$DOMAIN_NAME.cert $USER@$DESTINATION_SERVER:$DESTINATION_PATH$DESTINATION_CER_NAME
 
 #clean up pfsense
-rm /cf/conf/acme/$CERT_NAME.cert
+rm /tmp/acme/$CERT_NAME/$DOMAIN_NAME/$DOMAIN_NAME.cert
